6cfc03baa643c495c616188d24b956fb649a7de4
Justin <info@justinlmartin.com>
2023-11-09 12:00:27 -0800

    initial

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>Welcome</h3>
+        <p>
+            Back and Forth Designs and Dry Goods is a textile and clothing design studio based in Portland, Oregon. 
+            We are part of the Bridge Rat Studios Collective under the Morrison Bridge.
+        </p>
+    </div>
+</template>
+
+<style>
+
+/* @media (min-width: 1024px) {
+  .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+  }
+} */
+</style>
---
+++ src/views/MeetingView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="view-container">
+        <div>
+            <h3>Schedule a Meeting</h3>
+            <p>If you would like to schedule a meeting to talk about textiles, sewing or dry goods, feel free to put some time on my calendar.</p>
+        </div>
+        <iframe
+            id="meeting-scheduler"
+            src="https://app.simplymeet.me/justinlmartin/meeting?is_widget=1&view=compact&specific-meeting-type=1" 
+            frameborder="0" 
+            scrolling="yes"
+        ></iframe>
+    </div>
+</template>
+  
+<style>
+
+#meeting-scheduler {
+    margin-top: 3rem;
+    box-sizing: border-box;
+    /* padding: 0 2rem; */
+    width: 100%; 
+    height: 650px; 
+}
+
+@media (max-width: 600px) {
+    #meeting-scheduler {
+        margin-top: 1rem;
+        height: 1000px;
+    }
+}
+</style>
