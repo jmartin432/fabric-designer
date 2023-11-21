fd0b24296a85c1749594fcf87e0a8dae5a05f6d3
Justin <info@justinlmartin.com>
2023-11-21 10:21:56 -0800

    added cosine interpolation

--- src/modules/perlinUtils.js
+++ src/modules/perlinUtils.js
@@ -4,9 +4,11 @@
 //https://adrianb.io/2014/08/09/perlinnoise.html -->
 //https://sighack.com/post/getting-creative-with-perlin-noise-fields -->
 //https://gamedev.stackexchange.com/questions/197861/how-to-handle-octave-frequency-in-the-perlin-noise-algorithm -->
-///https://thebookofshaders.com/13/ -->
+//https://thebookofshaders.com/13/ -->
 //https://stackoverflow.com/questions/17427461/perlin-noise-value-range -->
 //https://rmarcus.info/blog/2018/03/04/perlin-noise.html -->
+//https://eev.ee/blog/2016/05/29/perlin-noise/ -->
+//https://joeiddon.github.io/projects/javascript/perlin.html -->
 
 const makeGrid = (n) => {
     let yArray = [];
