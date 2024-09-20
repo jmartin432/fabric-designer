286ffffefc90257f17b1aaa132278c7b06e881ac
Justin <info@justinlmartin.com>
2024-09-20 15:03:25 -0700

    yikes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,14 +2,14 @@
   <div>
         <h3>Welcome</h3>
         <p>
-            We love to sew and make clothes, but we also like to dream about algorithmically designing textiles.
+            I love to sew and make clothes, but I also like to dream about algorithmically designing textiles.
             This page is a step toward that. The Perlin Noise page lets you create a repeating pattern from Perlin Noise. Play around
         </p>
         <p>
             The source code can be found <a href="https://github.com/jmartin432/fabric-designer" target="_blank">here.</a>
         </p>
         <p>
-            If you would like to get in touch with us, you can do so <a href="https://backandforthdesigns.com/contact" target="_blank">here.</a>
+            If you would like to get in touch with me, you can do so <a href="https://backandforthdesigns.com/contact" target="_blank">here.</a>
         </p>
     </div>
 </template>
